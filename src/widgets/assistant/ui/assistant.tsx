1263d43fbe1333dc4e5e2119526e7c8df90d5341
EraEKV <kokenaoerasyl@gmail.com>
2024-09-25 01:41:12 +0500

    [feature]: chat logics

--- src/widgets/assistant/ui/assistant.tsx
+++ src/widgets/assistant/ui/assistant.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Input} from "@/shared/ui/input.tsx";
 import {Message} from "@/shared/ui/message.tsx";
 
+type MessageType = {
+    id: string;
+    chatId: string;
+    isUser: boolean;
+    content: string;
+};
+
 export const Assistant = () => {
-    const messages = [{
+    const messagesTest = [{
         id: "msg1",
         chatId: "chat123",
         isUser: true,
@@ -64,6 +71,31 @@ export const Assistant = () => {
             content: "Отлично, давайте рассмотрим каждую сущность по очереди."
         }]
 
+        const [message, setMessage] = useState("");
+        const [messages, setMessages] = useState<MessageType[]>(messagesTest);
+
+        const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+            if (event.key === "Enter") {
+                sendMessage();
+            }
+        };
+
+        const sendMessage = () => {
+            if (message.trim() !== "") {
+                const newMessage: MessageType = {
+                    id: `msg${messages.length + 1}`,
+                    chatId: "chat123",
+                    isUser: true,
+                    content: message
+                };
+
+                // Update the messages state
+                setMessages((prevMessages) => [...prevMessages, newMessage]);
+
+                // Clear the input field
+                setMessage("");
+            }
+        }
 
     return (
         <div
@@ -73,15 +105,21 @@ export const Assistant = () => {
                     <Message content={message.content} isUser={message.isUser} key={index}/>
                 ))
             }
-            <div className={'sticky flex justify-center items-center bottom-0 bg-white h-[20rem] rounded-lg'}>
-                <Input className={'text-black placeholder-black'}
-                       placeholder={'Напишите AI помошнику'}/>
-                <div className={"mr-5"}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                         className="bi bi-send-fill" viewBox="0 0 16 16">
-                        <path
-                            d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
-                    </svg>
+            <div className={'sticky h-[20rem]'}>
+                <div className={"relative flex justify-center items-center bottom-0 bg-white rounded-lg"}>
+                    <Input
+                        onChange={(e) => setMessage(e.target.value)}
+                        onKeyDown={handleKeyDown}
+                        className={'text-black py-5 pr-20 rounded-lg'}
+                        value={message}
+                        placeholder={'Напишите AI помошнику'}/>
+                    <div onClick={() => sendMessage()} className={"absolute right-5"}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="size-5 text-customlightgray cursor-pointer" viewBox="0 0 16 16">
+                            <path
+                                d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
--- src/widgets/chatMob/ui/assistantMob.tsx
+++ src/widgets/chatMob/ui/assistantMob.tsx
@@ -2,9 +2,15 @@ import {Message} from "@/shared/ui/message.tsx";
 import {Input} from "@/shared/ui/input.tsx";
 import React, {useState} from "react";
 
+type MessageType = {
+    id: string;
+    chatId: string;
+    isUser: boolean;
+    content: string;
+};
 
 export const AssistantMob = () => {
-    const messages = [{
+    const messagesTest = [{
         id: "msg1",
         chatId: "chat123",
         isUser: true,
@@ -65,6 +71,32 @@ export const AssistantMob = () => {
             content: "Отлично, давайте рассмотрим каждую сущность по очереди."
         }]
 
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState<MessageType[]>(messagesTest);
+
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            sendMessage();
+        }
+    };
+
+    const sendMessage = () => {
+        if (message.trim() !== "") {
+            const newMessage: MessageType = {
+                id: `msg${messages.length + 1}`,
+                chatId: "chat123",
+                isUser: true,
+                content: message
+            };
+
+            // Update the messages state
+            setMessages((prevMessages) => [...prevMessages, newMessage]);
+
+            // Clear the input field
+            setMessage("");
+        }
+    }
+
     const [isChatOpened, setIsChatOpened] = useState<boolean>(false)
     return (
         <div>
@@ -72,10 +104,21 @@ export const AssistantMob = () => {
                 <div
                     className={'absolute top-4 w-[95%] h-[95%] py-5 bg-customlightgray gap-y-3 left-1/2 transform -translate-x-1/2 rounded-xl border-solid border-gray-500 z-30'}>
                     <div className={"relative"}>
-                        <h3 className='text-center text-xl pb-4 border-b-black border-b-2'>Tokayev Online</h3>
+                        <div className={"flex justify-center gap-x-3"}>
+                            <h3 className='text-xl pb-4 border-b-black border-b-2'>Tokayev Online</h3>
+                            <div className={"pt-[6px]"}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                     className="text-blue-500" viewBox="0 0 16 16">
+                                    <path
+                                        d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708"/>
+                                </svg>
+                            </div>
+                        </div>
                         <div onClick={() => setIsChatOpened(false)} className={"absolute right-7 top-1 z-40"}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="size-6" viewBox="0 0 16 16">
-                                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="size-6" viewBox="0 0 16 16">
+                                <path
+                                    d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                             </svg>
                         </div>
                     </div>
@@ -86,9 +129,22 @@ export const AssistantMob = () => {
                                 <Message content={message.content} isUser={message.isUser} key={index}/>
                             ))
                         }
-                        <div className={'sticky flex justify-center items-center bottom-0 bg-white h-20 rounded-lg'}>
-                            <Input className={'text-black placeholder-black'}
-                                   placeholder={'Напишите AI помошнику'}/>
+                        <div className={'sticky h-[20rem]'}>
+                            <div className={"relative flex justify-center items-center bottom-0 bg-white rounded-lg"}>
+                            <Input
+                                    onChange={(e) => setMessage(e.target.value)}
+                                    onKeyDown={handleKeyDown}
+                                    className={'text-black py-5 pr-20'}
+                                    value={message}
+                                    placeholder={'Напишите AI помошнику'}/>
+                                <div onClick={() => sendMessage()} className={"absolute right-5"}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         className="size-5 text-customlightgray cursor-pointer" viewBox="0 0 16 16">
+                                        <path
+                                            d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
+                                    </svg>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
